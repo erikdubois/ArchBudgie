9d76775d1dfdb036913f18d5b8377af9fdb75b65
Erik Dubois <erik.dubois@gmail.com>
2017-01-14 00:07:55 +0100

    comment:

---
+++ 9-keyboard-shortcuts.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#======================================================================================
+#
+#                  -`
+#                  .o+`                 
+#                 `ooo/                  
+#                `+oooo:                 M  a  d  e    on 
+#               `+oooooo:               
+#               -+oooooo+:              
+#             `/:-:++oooo+:             
+#            `/++++/+++++++:                      A  R  C  H
+#           `/++++++++++++++:           
+#          `/+++ooooooooooooo/`         
+#         ./ooosssso++osssssso+`        
+#        .oossssso-````/ossssss+`       
+#       -osssssso.      :ssssssso.          F  o   r   
+#      :osssssss/        osssso+++.     
+#     /ossssssss/        +ssssooo/-     
+#   `/ossssso+/:-        -:/+osssso+-                           A   R   C   H
+#  `+sso+:-`                 `.-/+oso: 
+# `++:.                           `-/+/
+# .`                                 `/
+#
+#======================================================================================
+
+# Installation of zsh and oh-my-zsh
+
+echo "################################################################"
+echo "installing xbindkeys"
+echo "################################################################"
+
+sudo pacman -S xbindkeys --noconfirm
+
+
+echo "You need to put the config file in the root of your home folder"
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
