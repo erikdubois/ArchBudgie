4063ee0c327347e1083d3c696db1e58d16e893d7
Erik Dubois <erik.dubois@gmail.com>
2017-01-17 12:27:47 +0100

    comment:

--- _0-install-desktop-environment.sh
+++ _0-install-desktop-environment.sh
@@ -16,12 +16,12 @@ echo " Xserver setup"
 sudo pacman -S xorg-server xorg-server-utils xorg-xinit xorg-twm xterm --noconfirm
 sudo pacman -S x86-video-nouveau --noconfirm
 
+echo "Desktop Environment"
+
+sudo pacman -S budgie-desktop gnome --noconfirm
+
 echo "Display manager"
 
 sudo pacman -S gdm
 sudo systemctl enable gdm.service
 sudo systemctl start gdm.service
-
-echo "Desktop Environment"
-
-sudo pacman -S budgie-desktop gnome --noconfirm
