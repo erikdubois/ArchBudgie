a381796a3f0294f15430681af963b8db4aa94478
Erik Dubois <erik.dubois@gmail.com>
2017-01-27 22:08:49 +0100

    Comment :  on 22:08:49 27-01-2017

--- installation/600-install-personal-settings-v3.sh
+++ installation/600-install-personal-settings-v3.sh
@@ -37,14 +37,14 @@ echo "Creating all folders"
 echo "Moving files to start i3wm"
 echo "Using Sardi icons, Arc theme and noto font from previous scripts"
 
-cp settings/.bash_profile ~/
-cp settings/.bashrc ~/
-cp settings/.gtkrc-2.0 ~/
-cp settings/.xinitrc ~/
-cp settings/.zlogin ~/
-cp settings/.zshrc ~/
+#cp settings/.bash_profile ~/
+#cp settings/.bashrc ~/
+#cp settings/.gtkrc-2.0 ~/
+#cp settings/.xinitrc ~/
+#cp settings/.zlogin ~/
+#cp settings/.zshrc ~/
 cp settings/bookmarks ~/.config/gtk-3.0/
-cp settings/settings.ini ~/.config/gtk-3.0/
+#cp settings/settings.ini ~/.config/gtk-3.0/
 
 
 echo "Copy/pasting Arc Colora themes to ~/.themes"
