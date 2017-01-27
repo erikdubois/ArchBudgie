e9be2de9ca123f813815bfb18339dddc13d8d8c5
Erik Dubois <erik.dubois@gmail.com>
2017-01-27 14:33:57 +0100

    Comment :  on 14:33:57 27-01-2017

--- installation/050-install-budgie-core-v1.sh
+++ installation/050-install-budgie-core-v1.sh
@@ -73,7 +73,7 @@ fi
 # gnome
 
 echo "################################################################"
-echo "gnome"   
+echo "gnome and gdm"   
 echo "################################################################"
 
 package="gnome"
@@ -107,95 +107,17 @@ else
 			  	
 	fi
 
-	# Just checking if installation was successful
-	if pacman -Qi $package &> /dev/null; then
-	
-	echo "################################################################"
-	echo "#########  "$package" has been installed"
-	echo "################################################################"
-
-	else
-
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-	echo "!!!!!!!!!  "$package" has NOT been installed"
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
-	fi
 
 fi
 
 
 
-
-
-
-
-# alternative menu faster and only desktop apps
-
-echo "################################################################"
-echo "gdm"   
-echo "################################################################"
-
-package="gdm"
-
-#----------------------------------------------------------------------------------
-
-#checking if application is already installed or else install with aur helpers
-if pacman -Qi $package &> /dev/null; then
-
-	echo "################################################################"
-	echo "################## "$package" is already installed"
-	echo "################################################################"
-
-else
-
-	#checking which helper is installed
-	if pacman -Qi packer &> /dev/null; then
-
-		echo "Installing with packer"
-		packer -S --noconfirm --noedit  $package
-
-	elif pacman -Qi pacaur &> /dev/null; then
-		
-		echo "Installing with pacaur"
-		pacaur -S --noconfirm --noedit  $package
-		 	
-	elif pacman -Qi yaourt &> /dev/null; then
-
-		echo "Installing with yaourt"
-		yaourt -S --noconfirm $package
-			  	
-	fi
-
-	# Just checking if installation was successful
-	if pacman -Qi $package &> /dev/null; then
-	
-	echo "################################################################"
-	echo "#########  "$package" has been installed"
-	echo "################################################################"
-
-	else
-
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-	echo "!!!!!!!!!  "$package" has NOT been installed"
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
-	fi
-
-fi
-
-
-
-echo "Display manager being installed"
+echo "Display manager being activated"
 
 sudo systemctl enable gdm.service
 
-echo "Rebooting -select the proper desktop environment"
+echo "Reboot and select the proper desktop environment"
 echo "with the gauge symbol."
-
-sleep 3
-
-
 
 echo "################################################################"
 echo "###################    T H E   E N D      ######################"
