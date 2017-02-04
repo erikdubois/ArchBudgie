0126b8cf875268048d5dcd57fbe58575fbc9ffb3
Erik Dubois <erik.dubois@gmail.com>
2017-02-04 07:47:35 +0100

    Comment :  on Sat  4 Feb 07:47:35 CET 2017

--- git.sh
+++ git.sh
@@ -36,17 +36,17 @@
 
 # checking if kernel files are present otherswise github will become too big
 
-
 if [ -f linux* ]; then
 	echo "####################################"
-    echo "Stopping the script!!"
-    echo "Wait for the kernel update script to quit."
-    echo "####################################"
-    exit 0
+    	echo "Stopping the script!!"
+    	echo "Wait for the kernel update script to quit."
+    	echo "####################################"
+    	exit 0
 fi
 
-# checking if I have the latest files from github
 
+# checking if I have the latest files from github
+echo "Checking for newer files online first"
 git pull
 
 # Below command will backup everything inside the project folder
