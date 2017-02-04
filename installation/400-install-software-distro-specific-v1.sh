8d8d436210c18fed68861a2ccd5922bf6db9e99a
Erik Dubois <erik.dubois@gmail.com>
2017-02-04 09:35:39 +0100

    Comment :  on Sat Feb  4 09:35:39 CET 2017

--- installation/400-install-software-distro-specific-v1.sh
+++ installation/400-install-software-distro-specific-v1.sh
@@ -25,14 +25,11 @@ sudo pacman -S noto-fonts --noconfirm --needed
 
 #file manager
 
-#sudo pacman -S nemo --noconfirm --needed
+#sudo pacman -S nemo nemo-share nemo-fileroller --noconfirm --needed
 
+# extra extensions to compare files if needed install it
+# packer -S nemo-compare
 
-echo "You can install samba - folder sharing over network in next script"
-echo "You can copy my personal settings to your system or "
-
-echo "You can reboot now and start computing"
-echo "You need to login into the correct desktop environment"
-echo "At the login screen click on the small icon (gauge) to select budgie"
-echo "Type in the terminal the following code if you want to reboot"
-echo "sudo reboot"
+echo "################################################################"
+echo "#########   distro specific software installed  ################"
+echo "################################################################"
